9577e60bb14650445c7ecae29aaea749306bbc9c
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-11-20 09:27:09 +0530

    Added some theme to add some life in the app

---
+++ apps/web/app/providers/mantine-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { shadcnTheme } from "../theme/shadcnTheme";
+import { shadcnCssVariableResolver } from "../theme/cssVariableResolver";
+import "./style.css";
+
+
+
+export function MantineCustomProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <MantineProvider
+            defaultColorScheme="auto"
+            theme={shadcnTheme}
+            cssVariablesResolver={shadcnCssVariableResolver}
+        >
+            <Notifications />
+            {children}
+        </MantineProvider>
+    );
+}
