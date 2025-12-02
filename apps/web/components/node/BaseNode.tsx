d61e211a1a70db5788d6c3676643f84151d70380
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-12-02 11:15:32 +0530

    base node fixed and applied on custom node

--- apps/web/components/node/BaseNode.tsx
+++ apps/web/components/node/BaseNode.tsx
@@ -1,51 +1,63 @@
-import { ActionIcon, Group, Stack, Text } from "@mantine/core";
+import { ActionIcon, Box, Group, Stack, Text } from "@mantine/core";
 import { IconEdit, IconTrash } from "@tabler/icons-react";
 import { ReactNode } from "react";
 
 export interface BaseNodeProps {
-  children: ReactNode;
-  showToolbar?: boolean;
-  onNodeDelete?: () => void;
-  onNodeEdit?: () => void;
-  name?: string;
-  descritpion?: string;
+    children: ReactNode;
+    showToolbar?: boolean;
+    onNodeDelete?: () => void;
+    onNodeEdit?: () => void;
+    name?: string;
+    descritpion?: string;
 }
 
 export default function BaseNode({
-  showToolbar,
-  onNodeEdit,
-  onNodeDelete,
-  name,
-  descritpion,
-  children,
+    showToolbar,
+    onNodeEdit,
+    onNodeDelete,
+    name,
+    descritpion,
+    children,
 }: BaseNodeProps) {
-  return (
-    <Stack align="center" gap={3}>
-      {showToolbar && (
-        <Group gap={5}>
-          <ActionIcon variant="subtle" size={10} onClick={onNodeEdit}>
-            <IconEdit />
-          </ActionIcon>
-          <ActionIcon variant="subtle" size={10} onClick={onNodeDelete}>
-            <IconTrash />
-          </ActionIcon>
-        </Group>
-      )}
+    return (
+        <Stack align="center">
+            <Stack
+                pos="relative"
+                gap={0}
+            >
+                {showToolbar && (
+                    <Group
+                        gap={0}
+                        pos="absolute"
+                        top={-20}
+                        right={2}
+                    >
+                        <ActionIcon variant="subtle" color="cyan" onClick={onNodeEdit} size={"xs"}>
+                            <IconEdit size={10} />
+                        </ActionIcon>
 
-      {children}
+                        <ActionIcon variant="subtle" color="cyan" onClick={onNodeDelete} size={"xs"}>
+                            <IconTrash size={10} />
+                        </ActionIcon>
+                    </Group>
+                )}
 
-      {name && (
-        <Stack
-          style={{ textAlign: "center", pointerEvents: "none" }}
-          gap={2}
-          align="center"
-        >
-          <Text size="xs" fw={600}>
-            {name}
-          </Text>
-          <Text size="xs">{descritpion}</Text>
+                {children}
+            </Stack>
+
+            <Stack
+                gap={3}
+                align="center"
+                style={{ textAlign: "center", pointerEvents: "none" }}
+            >
+                <Text fw={700} size="xs">
+                    {name}
+                </Text>
+
+                <Text size="xs" c="dimmed" truncate="end">
+                    {descritpion}
+                </Text>
+            </Stack>
         </Stack>
-      )}
-    </Stack>
-  );
+    );
 }
--- apps/web/components/node/CustomNode.tsx
+++ apps/web/components/node/CustomNode.tsx
@@ -1,17 +1,17 @@
 import { CustomNodeType, SupportedCredential } from "@/utils";
 import {
-  ActionIcon,
-  Button,
-  Group,
-  Modal,
-  MultiSelect,
-  NumberInput,
-  PasswordInput,
-  Select,
-  Stack,
-  Text,
-  TextInput,
-  Title,
+    ActionIcon,
+    Button,
+    Group,
+    Modal,
+    MultiSelect,
+    NumberInput,
+    PasswordInput,
+    Select,
+    Stack,
+    Text,
+    TextInput,
+    Title,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useDisclosure } from "@mantine/hooks";
@@ -20,155 +20,165 @@ import { Handle, Position, useReactFlow, type NodeProps } from "@xyflow/react";
 import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import BaseNode from "./BaseNode";
 
 export default function CustomNode({ data, id }: NodeProps<CustomNodeType>) {
-  const { updateNodeData } = useReactFlow();
-  const [opened, { close, open }] = useDisclosure();
-  const [selectedCredential, setSelectedCredendtial] = useState<string | null>(
-    null
-  );
-  const form = useForm({
-    mode: "uncontrolled",
-  });
+    const { updateNodeData } = useReactFlow();
+    const [opened, { close, open }] = useDisclosure();
+    const [selectedCredential, setSelectedCredendtial] = useState<string | null>(
+        null
+    );
+    const form = useForm({
+        mode: "uncontrolled",
+    });
 
-  const [supportedCredentials, setSupportedCredentials] = useState<
-    SupportedCredential[]
-  >([]);
+    const [supportedCredentials, setSupportedCredentials] = useState<
+        SupportedCredential[]
+    >([]);
 
-  const handleSubmit = (values: Record<string, unknown>) => {
-    if (selectedCredential) {
-      updateNodeData(id, {
-        ...data,
-        parameter: values,
-        credentialId: selectedCredential,
-      });
-    }
-  };
-
-  useEffect(() => {
-    const getSupportedCredentials = async () => {
-      const response = await axios.get(
-        `/api/credential/supported?type=${data.nodeSchema.type}`
-      );
-      const supportedCredentials = response.data
-        .supportedCredentials as SupportedCredential[];
-
-      setSupportedCredentials(supportedCredentials);
+    const handleSubmit = (values: Record<string, unknown>) => {
+        if (selectedCredential) {
+            updateNodeData(id, {
+                ...data,
+                parameter: values,
+                credentialId: selectedCredential,
+            });
+        }
     };
 
-    getSupportedCredentials();
-  }, [data.nodeSchema.type]);
+    useEffect(() => {
+        const getSupportedCredentials = async () => {
+            const response = await axios.get(
+                `/api/credential/supported?type=${data.nodeSchema.type}`
+            );
+            const supportedCredentials = response.data
+                .supportedCredentials as SupportedCredential[];
 
-  return (
-    <>
-      <Group>
-        <ActionIcon
-          onClick={open}
-          variant="white"
-          bd={"1px solid black"}
-          size={40}
-        >
-          <Image
-            src={data.nodeSchema.iconUrl ?? ""}
-            alt={data.nodeSchema.name}
-            width={20}
-            height={20}
-          />
-        </ActionIcon>
-        <Handle id="main" type="target" position={Position.Left} />
-        <Handle id="main" type="source" position={Position.Right} />
-      </Group>
-      <Modal
-        centered
-        opened={opened}
-        onClose={close}
-        title={
-          <Stack gap={1}>
-            <Title component={"div"} order={1}>
-              {data.nodeSchema.displayName}
-            </Title>
-            <Text c="dimmed">{data.nodeSchema.description}</Text>
-          </Stack>
-        }
-      >
-        <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
-          <Stack>
-            {data.nodeSchema.requiredCredential ? (
-              <Select
-                label="Credential"
-                data={supportedCredentials.map((cred) => ({
-                  value: cred.id,
-                  label: cred.name,
-                }))}
-                value={selectedCredential}
-                onChange={setSelectedCredendtial}
-                required
-              />
-            ) : null}
-            {data.nodeSchema.properties.map((property) => {
-              switch (property.type) {
-                case PropertyTypes.string:
-                  return !property.typeOptions?.password ? (
-                    <TextInput
-                      label={property.displayName}
-                      placeholder={property.placeholder}
-                      required={property.required}
-                      description={property.description}
-                      key={form.key(property.name)}
-                      name={property.name}
-                      {...form.getInputProps(property.name)}
-                    />
-                  ) : (
-                    <PasswordInput
-                      label={property.displayName}
-                      placeholder={property.placeholder}
-                      required={property.required}
-                      description={property.description}
-                      key={form.key(property.name)}
-                      name={property.name}
-                      {...form.getInputProps(property.name)}
-                    />
-                  );
+            setSupportedCredentials(supportedCredentials);
+        };
 
-                case PropertyTypes.number:
-                  return (
-                    <NumberInput
-                      key={form.key(property.name)}
-                      label={property.displayName}
-                      placeholder={property.placeholder}
-                      required={property.required}
-                      description={property.description}
-                      name={property.name}
-                      {...form.getInputProps(property.name)}
+        getSupportedCredentials();
+    }, [data.nodeSchema.type]);
+
+    return (
+        <>
+            <BaseNode
+                showToolbar={true}
+                name={data.nodeSchema.displayName}
+                descritpion={data.nodeSchema.description}
+            >
+                <ActionIcon
+                    onClick={open}
+                    variant="white"
+                    bd={"1px solid black"}
+                    size={40}
+                >
+                    <Image
+                        src={data.nodeSchema.iconUrl ?? ""}
+                        alt={data.nodeSchema.name}
+                        width={20}
+                        height={20}
                     />
-                  );
-                case PropertyTypes.multiSelect:
-                  return (
-                    <MultiSelect
-                      key={form.key(property.name)}
-                      data={property.options?.map((option) => ({
-                        value: option.name ?? "",
-                        label: option.displayName ?? "",
-                      }))}
-                      label={property.displayName}
-                      placeholder={property.placeholder}
-                      required={property.required}
-                      description={property.description}
-                      name={property.name}
-                      {...form.getInputProps(property.name)}
-                    />
-                  );
-              }
-            })}
-          </Stack>
-          <Group justify="flex-end" mt="lg">
-            <Button type="submit">Save</Button>
-            <Button color="red" onClick={() => close()}>
-              Cancel
-            </Button>
-          </Group>
-        </form>
-      </Modal>
-    </>
-  );
+                </ActionIcon>
+
+                {/* FIX: Place handles here */}
+                {data.nodeSchema.executionType === "action" && (
+                    <Handle id="main-target" type="target" position={Position.Left} />
+                )}
+                <Handle id="main-source" type="source" position={Position.Right} />
+            </BaseNode>
+
+            <Modal
+                centered
+                opened={opened}
+                onClose={close}
+                title={
+                    <Stack gap={1}>
+                        <Title component={"div"} order={1}>
+                            {data.nodeSchema.displayName}
+                        </Title>
+                        <Text c="dimmed">{data.nodeSchema.description}</Text>
+                    </Stack>
+                }
+            >
+                <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+                    <Stack>
+                        {data.nodeSchema.requiredCredential ? (
+                            <Select
+                                label="Credential"
+                                data={supportedCredentials.map((cred) => ({
+                                    value: cred.id,
+                                    label: cred.name,
+                                }))}
+                                value={selectedCredential}
+                                onChange={setSelectedCredendtial}
+                                required
+                            />
+                        ) : null}
+                        {data.nodeSchema.properties.map((property) => {
+                            switch (property.type) {
+                                case PropertyTypes.string:
+                                    return !property.typeOptions?.password ? (
+                                        <TextInput
+                                            label={property.displayName}
+                                            placeholder={property.placeholder}
+                                            required={property.required}
+                                            description={property.description}
+                                            key={form.key(property.name)}
+                                            name={property.name}
+                                            {...form.getInputProps(property.name)}
+                                        />
+                                    ) : (
+                                        <PasswordInput
+                                            label={property.displayName}
+                                            placeholder={property.placeholder}
+                                            required={property.required}
+                                            description={property.description}
+                                            key={form.key(property.name)}
+                                            name={property.name}
+                                            {...form.getInputProps(property.name)}
+                                        />
+                                    );
+
+                                case PropertyTypes.number:
+                                    return (
+                                        <NumberInput
+                                            key={form.key(property.name)}
+                                            label={property.displayName}
+                                            placeholder={property.placeholder}
+                                            required={property.required}
+                                            description={property.description}
+                                            name={property.name}
+                                            {...form.getInputProps(property.name)}
+                                        />
+                                    );
+                                case PropertyTypes.multiSelect:
+                                    return (
+                                        <MultiSelect
+                                            key={form.key(property.name)}
+                                            data={property.options?.map((option) => ({
+                                                value: option.name ?? "",
+                                                label: option.displayName ?? "",
+                                            }))}
+                                            label={property.displayName}
+                                            placeholder={property.placeholder}
+                                            required={property.required}
+                                            description={property.description}
+                                            name={property.name}
+                                            {...form.getInputProps(property.name)}
+                                        />
+                                    );
+                            }
+                        })}
+                    </Stack>
+                    <Group justify="flex-end" mt="lg">
+                        <Button type="submit">Save</Button>
+                        <Button color="red" onClick={() => close()}>
+                            Cancel
+                        </Button>
+                    </Group>
+                </form>
+            </Modal>
+        </>
+    );
 }
