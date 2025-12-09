07729b63fbe1cf05a7d1ae264c0529ed7df6a218
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-12-09 10:33:50 +0530

    Added loading skeleton

---
+++ apps/web/components/loadingSkeleton/index.tsx
@@ -0,0 +1,13 @@
+import { Skeleton, SkeletonProps } from "@mantine/core";
+
+export interface LoadingSkeletonProps extends SkeletonProps {
+    lenght: number;
+}
+export default function LoadingSkeleton({
+    lenght,
+    ...rest
+}: LoadingSkeletonProps) {
+    return Array.from({ length: lenght }).map((_, idx) => (
+        <Skeleton key={idx} {...rest} />
+    ));
+}
--- apps/web/components/workflows/index.tsx
+++ apps/web/components/workflows/index.tsx
@@ -5,14 +5,18 @@ import { useEffect, useState } from "react";
 import { Workflow } from "@w8w/db/prisma-browser";
 import axios from "axios";
 import Link from "next/link";
+import LoadingSkeleton from "../loadingSkeleton";
 
 export const Workflows = () => {
   const [workflows, setWorkflows] = useState<Workflow[]>([]);
+  const [loadingWorkflows, setLoadingWorkflows] = useState(false);
   useEffect(() => {
     const getWorkflow = async () => {
+      setLoadingWorkflows(true);
       const { data } = await axios.get<{ workflows: Workflow[] }>(
         `/api/workflow`,
       );
+      setLoadingWorkflows(false);
       setWorkflows(data.workflows);
     };
 
@@ -24,27 +28,31 @@ export const Workflows = () => {
       <Group justify="flex-end">
         <Button onClick={createWorkflow}>Create Workflow</Button>
       </Group>
-      <Stack gap={4}>
-        {workflows.map((workflow) => (
-          <Card
-            withBorder
-            shadow="xs"
-            className="border-2 transition-all duration-200 hover:scale-101 hover:shadow-lg cursor-pointer"
-            style={{ cursor: "pointer" }}
-            key={workflow.id}
-          >
-            <Link
-              href={{
-                pathname: `/workflow/${workflow.id}`,
-                query: { name: workflow.name },
-              }}
-              className="text-xl font-bold"
+      {loadingWorkflows ? (
+        <LoadingSkeleton lenght={5} height={70} />
+      ) : (
+        <Stack gap={4}>
+          {workflows.map((workflow) => (
+            <Card
+              withBorder
+              shadow="xs"
+              className="border-2 transition-all duration-200 hover:scale-101 hover:shadow-lg cursor-pointer"
+              style={{ cursor: "pointer" }}
+              key={workflow.id}
             >
-              {workflow.id}
-            </Link>
-          </Card>
-        ))}
-      </Stack>
+              <Link
+                href={{
+                  pathname: `/workflow/${workflow.id}`,
+                  query: { name: workflow.name },
+                }}
+                className="text-xl font-bold"
+              >
+                {workflow.id}
+              </Link>
+            </Card>
+          ))}
+        </Stack>
+      )}
     </Stack>
   );
 };
