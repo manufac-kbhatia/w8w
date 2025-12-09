0e7a6b4199fd9586fac6ba5c56b00c994fbb250f
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-12-09 10:59:47 +0530

    Added action button

--- apps/web/components/workflows/index.tsx
+++ apps/web/components/workflows/index.tsx
@@ -1,58 +1,76 @@
 "use client";
-import { Button, Card, Group, Stack } from "@mantine/core";
-import { createWorkflow } from "@/utils";
+import { ActionIcon, Button, Card, Divider, Group, Stack, Text, Title } from "@mantine/core";
+import { checkMannualTriggerExist, createWorkflow, formatUpdatedAt, transformNodes } from "@/utils";
 import { useEffect, useState } from "react";
 import { Workflow } from "@w8w/db/prisma-browser";
 import axios from "axios";
 import Link from "next/link";
 import LoadingSkeleton from "../loadingSkeleton";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
 
 export const Workflows = () => {
-  const [workflows, setWorkflows] = useState<Workflow[]>([]);
-  const [loadingWorkflows, setLoadingWorkflows] = useState(false);
-  useEffect(() => {
-    const getWorkflow = async () => {
-      setLoadingWorkflows(true);
-      const { data } = await axios.get<{ workflows: Workflow[] }>(
-        `/api/workflow`,
-      );
-      setLoadingWorkflows(false);
-      setWorkflows(data.workflows);
-    };
+    const [workflows, setWorkflows] = useState<Workflow[]>([]);
+    const [loadingWorkflows, setLoadingWorkflows] = useState(false);
+    useEffect(() => {
+        const getWorkflow = async () => {
+            setLoadingWorkflows(true);
+            const { data } = await axios.get<{ workflows: Workflow[] }>(
+                `/api/workflow`,
+            );
+            setLoadingWorkflows(false);
+            setWorkflows(data.workflows);
+        };
 
-    getWorkflow();
-  }, []);
+        getWorkflow();
+    }, []);
 
-  return (
-    <Stack>
-      <Group justify="flex-end">
-        <Button onClick={createWorkflow}>Create Workflow</Button>
-      </Group>
-      {loadingWorkflows ? (
-        <LoadingSkeleton lenght={5} height={70} />
-      ) : (
-        <Stack gap={4}>
-          {workflows.map((workflow) => (
-            <Card
-              withBorder
-              shadow="xs"
-              className="border-2 transition-all duration-200 hover:scale-101 hover:shadow-lg cursor-pointer"
-              style={{ cursor: "pointer" }}
-              key={workflow.id}
-            >
-              <Link
-                href={{
-                  pathname: `/workflow/${workflow.id}`,
-                  query: { name: workflow.name },
-                }}
-                className="text-xl font-bold"
-              >
-                {workflow.id}
-              </Link>
-            </Card>
-          ))}
+    return (
+        <Stack>
+            <Group justify="flex-end">
+                <Button onClick={createWorkflow}>Create Workflow</Button>
+            </Group>
+            {loadingWorkflows ? (
+                <LoadingSkeleton lenght={5} height={70} />
+            ) : (
+                <Stack gap={4}>
+                    {workflows.map((workflow) => (
+                        <Link
+                            key={workflow.id}
+                            href={{
+                                pathname: `/workflow/${workflow.id}`,
+                                query: { name: workflow.name },
+                            }}
+                        >
+                            <Card
+                                withBorder
+                                shadow="xs"
+                                className="border-2 transition-all duration-200 hover:scale-101 hover:shadow-lg cursor-pointer"
+                                style={{ cursor: "pointer" }}
+                            >
+                                <Group justify="space-between" align="center">
+                                    <Stack gap={0}>
+                                        <Title order={1}>{workflow.name}</Title>
+                                        <Group>
+                                            <Text>
+                                                Last updated at {formatUpdatedAt(workflow.updatedAt)}
+                                            </Text>
+                                            <Divider orientation="vertical" size="sm" />
+                                            <Text>
+                                                Created at {formatUpdatedAt(workflow.createdAt)}
+                                            </Text>
+                                        </Group>
+                                    </Stack>
+                                    <Group>
+                                        <Button disabled={!checkMannualTriggerExist(transformNodes(workflow.nodes))} size="compact-md">Execute</Button>
+                                        <ActionIcon size="md" radius="md" variant="light"><IconEdit size={20} /></ActionIcon>
+                                        <ActionIcon size="md" radius="md" variant="light"><IconTrash size={20} /></ActionIcon>
+                                    </Group>
+                                </Group>
+                            </Card>
+                        </Link>
+                    ))}
+                </Stack>
+            )}
         </Stack>
-      )}
-    </Stack>
-  );
+    );
 };
