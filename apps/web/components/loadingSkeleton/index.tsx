0b6eea09638fd74d06e26bc88ab44108b17821a3
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-12-09 10:35:04 +0530

    husky changes

--- apps/web/components/loadingSkeleton/index.tsx
+++ apps/web/components/loadingSkeleton/index.tsx
@@ -1,13 +1,13 @@
 import { Skeleton, SkeletonProps } from "@mantine/core";
 
 export interface LoadingSkeletonProps extends SkeletonProps {
-    lenght: number;
+  lenght: number;
 }
 export default function LoadingSkeleton({
-    lenght,
-    ...rest
+  lenght,
+  ...rest
 }: LoadingSkeletonProps) {
-    return Array.from({ length: lenght }).map((_, idx) => (
-        <Skeleton key={idx} {...rest} />
-    ));
+  return Array.from({ length: lenght }).map((_, idx) => (
+    <Skeleton key={idx} {...rest} />
+  ));
 }
